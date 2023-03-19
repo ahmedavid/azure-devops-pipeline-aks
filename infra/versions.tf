6157cea2326fd323f845bf448af7732011ce492c
David Ahmadov <ahmedavid@gmail.com>
2023-03-19 09:38:51 +0400

    infra

---
+++ infra/versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">=1.3.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.36.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+resource "random_pet" "aksrandom" {
+
+}
